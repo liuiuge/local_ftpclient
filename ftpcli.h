7c395a345d9b1d9cf7bcdb25b1857f18f1ca7f67
liuiuge <liuiuge@users.noreply.github.com>
2017-06-03 10:36:47 +0800

    add some new function
    unfortunately i forget what added

---
+++ ftpcli.h
@@ -0,0 +1,24 @@
+#ifndef __FTPCLI_H__
+#define __FTPCLI_H__
+#include "head.h"
+
+//客户端功�?
+int cli_cd(int );
+int cli_ls(int );
+int cli_remove(int);
+int cli_get(int);
+int cli_put(int);
+
+char *GetFileSHA1b(char *, char *, char *);
+
+int send_n(int, char *, int);
+int send_file(int);
+void recv_n(int,char *,int);
+void cli_crypt(char *,char *);
+
+typedef struct data_send{
+	int len;
+	char buf[1000];
+}train,*ptrain;
+
+#endif
